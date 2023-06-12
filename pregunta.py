716c9917f78bfad7eedce47373b9e2e7a2e2d0af
Yezenia Restrepo <125924954+YezeniaR@users.noreply.github.com>
2023-06-11 22:10:47 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,16 +11,16 @@ import pandas as pd
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    # Eliminar las columnas duplicadas
     df = df.drop_duplicates()
     df.dropna(axis=0,inplace=True)
     df.drop_duplicates(inplace = True)
 
-    # Reemplazar los valores incorrectos o inconsistentes en las columnas
+    for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']: #['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'barrio', 'línea_credito']:
+        df[columna] = df[columna].apply(lambda x: x.lower()) 
+
     df["monto_del_credito"] = df["monto_del_credito"].str.replace("[^0-9]", "", regex=True)
     df["línea_credito"] = df["línea_credito"].str.lower()
 
-    # Manejar los datos faltantes
     df = df.dropna()
 
     return df
